1952ee135eb6b954770b5425aa8212645404bf1c
Perry Zhu <perryzhu2004@outlook.com>
2025-10-26 18:55:20 -0700

    feat: back to tokyo night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,72 +1,22 @@
 return {
-  -- 1. Tell LazyVim to use Catppuccin as the default colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "tokyonight",
     },
   },
 
   -- 2. Configure Catppuccin using the defaults you provided,
   --    but adding the auto-switching logic.
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
     priority = 1000, -- Make sure this loads first
     opts = {
-      -- ADDED: This is the magic for auto-switching
       flavour = "auto",
       background = {
-        light = "latte",
-        dark = "mocha",
-      },
-
-      -- KEPT: All the LazyVim defaults you referenced
-      lsp_styles = {
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
-        },
-      },
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        fzf = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        mini = true,
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        snacks = true,
-        telescope = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-    -- KEPT: The default spec for bufferline integration
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.special.bufferline").get_theme()
-          end
-        end,
+        light = "day",
+        dark = "storm",
       },
     },
   },
