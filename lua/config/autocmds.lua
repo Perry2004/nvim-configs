45269a61fa8110d1703fd1902fc9439c095d27c9
Perry Zhu <perryzhu2004@outlook.com>
2025-10-26 22:25:24 -0700

    feat: home command

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,8 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- home command
+vim.api.nvim_create_user_command("Home", function()
+  require("snacks").dashboard.open()
+end, { desc = "Open LazyVim Dashboard (Home)" })
